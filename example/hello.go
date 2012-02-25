4ecf700361698c9410889079e83f0ccac8175a00
George Rogers <georgerogers42@aol.com>
2012-02-25 13:33:54 -0600

    Update Readme

--- example/hello.go
+++ example/hello.go
@@ -1,26 +1,29 @@
 package main
 
 import (
-	"github.com/bmizerany/pat.go"
+	"github.com/georgerogers42/pat2"
 	"io"
 	"net/http"
 )
 
 func main() {
 	m := pat.New()
-	m.Get("/hello/:name", http.HandlerFunc(hello))
-	m.Get("/splat/*", http.HandlerFunc(splat))
+	m.Get("/hello/:name", pat.HandlerFunc(hello))
+	m.Get("/splat/", pat.HandlerFunc(splat))
 	http.ListenAndServe("localhost:5000", m)
 }
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	// Path variable names are in the URL.Query() and start with ':'.
-	name := r.URL.Query().Get(":name")
-	io.WriteString(w, "Hello, "+name)
+func hello(params pat.Params, _ string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Path variable names are in the URL.Query() and start with ':'.
+		name := params[":name"]
+		io.WriteString(w, "Hello, "+name)
+	}
 }
 
-func splat(w http.ResponseWriter, r *http.Request) {
-	// Path variable names are in the URL.Query() and start with ':'.
-	s := r.URL.Query().Get(":splat")
-	io.WriteString(w, "Splat: "+s)
+func splat(_ pat.Params, s string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Path variable names are in the URL.Query() and start with ':'.
+		io.WriteString(w, "Splat: "+s)
+	}
 }
--- mux.go
+++ mux.go
@@ -5,7 +5,14 @@ import (
 	"net/http"
 )
 
-type Handler func (params Params) http.Handler
+type Handler interface {
+	Handle(params Params, splat string) http.HandlerFunc
+}
+type HandlerFunc func (params Params, splat string) http.HandlerFunc
+func (h HandlerFunc) Handle(params Params, splat string) http.HandlerFunc {
+	return h(params, splat)
+}
+
 type Params map[string]string
 
 type PatternServeMux struct {
@@ -20,8 +27,8 @@ func New() *PatternServeMux {
 // Implements HttpHandler.
 func (p *PatternServeMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	for _, ph := range p.handlers[r.Method] {
-		if params, ok := ph.try(r.URL.Path); ok {
-			px := ph.handler(params)
+		if params, splat, ok := ph.try(r.URL.Path); ok {
+			px := ph.Handle(params, splat)
 			px.ServeHTTP(w, r)
 			return
 		}
@@ -57,20 +64,19 @@ func (p *PatternServeMux) Add(meth, pat string, h Handler) {
 
 type patHandler struct {
 	pat string
-	handler Handler
+	Handler
 }
 
-func (ph *patHandler) try(path string) (Params, bool) {
+func (ph *patHandler) try(path string) (Params, string, bool) {
 	p := make(Params)
 	var i, j int
 	for i < len(path) {
 		switch {
 		case j == len(ph.pat) && ph.pat[j-1] == '/':
 			// Should i put a special form variable splat for this case
-			p[":splat"] = path[i:]
-			return p, true
+			return p, path[i:], true
 		case j >= len(ph.pat):
-			return nil, false
+			return nil, "",false
 		case ph.pat[j] == ':':
 			var name, val string
 			name, j = find(ph.pat, '/', j)
@@ -80,13 +86,13 @@ func (ph *patHandler) try(path string) (Params, bool) {
 			i++
 			j++
 		default:
-			return nil, false
+			return nil, "", false
 		}
 	}
 	if j != len(ph.pat) {
-		return nil, false
+		return nil, "", false
 	}
-	return p, true
+	return p, "", true
 }
 
 func find(s string, c byte, i int) (string, int) {
